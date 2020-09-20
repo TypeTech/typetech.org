aaa247f4f4ad942cf8336fde0bf35b0c97abcbbd
rodzy <irod2899@gmail.com>
2020-09-19 19:46:21 -0600

    Subscribe form working ✅

--- src/components/contactform.js
+++ src/components/contactform.js
@@ -1,83 +1,69 @@
 import React, { useState } from "react"
-import styled from "@emotion/styled"
+import MailchimpSubscribe from "react-mailchimp-subscribe"
+import SubscribeForm from '../components/subscribeForm'
 
-const InputEmail = styled.input`
-  display: inline-block;
-  text-align: center;
-  letter-spacing: 0.026em;
-  cursor: pointer;
-  font-size: 0.875rem;
-  text-transform: none;
-  background-color: transparent;
-  color: #000000;
-  outline: none;
-  padding: 8px 1.6em;
-  text-decoration: none;
-  border-radius: 3px;
-  border-width: 1px;
-  border-style: solid;
-  border-color: #000000;
-  border-image: initial;
-  &:focus {
-    border-color: #4c9ac0;
-  }
-`
 
-const SubmitButton = styled.input`
-  margin: 20px;
-  padding: 10px;
-  width: 40%;
-  background-color: #4c9ac0;
-  border: none;
-  border-radius: 0.5rem;
-  font-size: 18px;
-  font-weight: 600;
-  color: #ffffff;
-  cursor:pointer;
-  &:hover{
-    transition:1s ease;
-    background-color:#1476a2;
-  }
-`
 
 const ContactForm = () => {
-  const [email, setEmail] = useState()
-  return (
-    <form
-      action="https://gmail.us10.list-manage.com/subscribe/post?u=87f1632117fc73582c5efd107&amp;id=3fb05439f2"
-      method="post"
-      className="validate"
-      target="_blank"
-      noValidate
-    >
-      <div id="mc_embed_signup_scroll">
-        <InputEmail
-          type="email"
-          value={email}
-          name="EMAIL"
-          placeholder="youremail@here.com"
-          required
-        />
+  const [email, setEmail] = useState("")
+  const url =
+    "https://gmail.us10.list-manage.com/subscribe/post?u=87f1632117fc73582c5efd107&id=3fb05439f2"
 
-        <div style={{ position: "absolute", left: -5000 }}>
-          <input
-            type="text"
-            name="b_87f1632117fc73582c5efd107_3fb05439f2"
-            tabIndex="-1"
-          />
-        </div>
-        <div className="clear">
-          <SubmitButton
-            type="submit"
-            value="Subscribe"
-            name="subscribe"
-            className="button"
-            onSubmit={() => setEmail()}
-          />
-        </div>
-      </div>
-    </form>
+  const handleChange = e => {
+    setEmail(e)
+  }
+  return (
+    <MailchimpSubscribe
+      url={url}
+      render={({ subscribe, status, message }) => (
+      
+          <SubscribeForm status={status} message={message} onValidated={formData => subscribe(formData)} />
+          
+          // <form onSubmit={formData => subscribe(formData)} className="validate">
+          //   <div id="mc_embed_signup_scroll">
+          //     <InputEmail
+          //       type="email"
+          //       value={email}
+          //       name="EMAIL"
+          //       placeholder="youremail@here.com"
+          //       onChange={e => handleChange(e.currentTarget.value)}
+          //       required
+          //     />
+
+          //     <div style={{ position: "absolute", left: -5000 }}>
+          //       <input
+          //         type="text"
+          //         name="b_87f1632117fc73582c5efd107_3fb05439f2"
+          //         tabIndex="-1"
+          //       />
+          //     </div>
+          //     <div className="clear">
+          //       <SubmitButton
+          //         type="submit"
+          //         value="Subscribe"
+          //         name="subscribe"
+          //         className="button"
+          //       />
+          //     </div>
+          //   </div>
+          // </form>
+          // {status === "sending" && (
+          //   <div style={{ color: "blue" }}>sending...</div>
+          // )}
+          // {status === "error" && (
+          //   <div
+          //     style={{ color: "red" }}
+          //     dangerouslySetInnerHTML={{ __html: message }}
+          //   />
+          // )}
+          // {status === "success" && (
+          //   <div style={{ color: "green" }}>Subscribed !</div>
+          
+      )}
+    />
   )
 }
 
 export default ContactForm
+
+
---
+++ src/components/subscribeForm.js
@@ -0,0 +1,90 @@
+import React from "react"
+import styled from "@emotion/styled"
+
+const InputEmail = styled.input`
+  display: inline-block;
+  text-align: center;
+  letter-spacing: 0.026em;
+  cursor: pointer;
+  font-size: 1rem;
+  text-transform: none;
+  background-color: transparent;
+  color: #000000;
+  outline: none;
+  padding: 10px 2em;
+  text-decoration: none;
+  border-radius: 3px;
+  border-width: 1px;
+  border-style: solid;
+  border-color: #000000;
+  border-image: initial;
+  &:focus {
+    border-color: #4c9ac0;
+  }
+`
+
+const SubmitButton = styled.button`
+  margin: 20px;
+  padding: 10px;
+  width: 10em;
+  background-color: #4c9ac0;
+  border: none;
+  border-radius: 0.5rem;
+  font-size: 1.5rem;
+  font-weight: 600;
+  color: #ffffff;
+  transition:background-color 1s ease;
+  &:hover {
+    cursor: pointer;
+    background-color: #1476a2;
+  }
+`
+
+const SubscribeForm = ({ status, message, onValidated }) => {
+  let email
+  const submit = () =>
+    email &&
+    email.value.indexOf("@") > -1 &&
+    onValidated({
+      EMAIL: email.value,
+    })
+
+  return (
+    <div
+      style={{
+        borderRadius: 2,
+        padding: 10,
+        display: "inline-block",
+      }}
+    >
+      {status === "sending" && <div style={{ color: "blue" }}>sending...</div>}
+      {status === "error" && (
+        <div
+          style={{ color: "red" }}
+          dangerouslySetInnerHTML={{ __html: message }}
+        />
+      )}
+      {status === "success" && (
+        <div
+          style={{ color: "green" }}
+          dangerouslySetInnerHTML={{ __html: message }}
+        />
+      )}
+      <br />
+      <InputEmail
+        ref={node => (email = node)}
+        type="email"
+        placeholder="Your email"
+      />
+      <br />
+      <SubmitButton
+        onClick={submit}
+        disabled={status === "sending" ? true : false}
+      >
+        {status === "sending" ? "Sending ..." : "Submit"}
+      </SubmitButton>
+    </div>
+  )
+}
+
+export default SubscribeForm
