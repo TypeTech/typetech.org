c6a2af08a073cf885c25a03e7b55dce6346e796c
rodzy <49137701+rodzy@users.noreply.github.com>
2020-09-20 22:27:20 -0600

    [Fix] Webpack builds fail with React Context 🎉

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,23 +2,28 @@ import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import styled from "@emotion/styled"
+import ModeProvider from "../context/ModeContext"
 
 const MainDiv = styled.div`
-  display:flex;
-  width:100%;
+  display: flex;
+  width: 100%;
   background-color: #333333;
-  justify-content:center;
-  color:gray;
-  position:fixed;
+  justify-content: center;
+  color: gray;
+  position: fixed;
 `
 
-const NotFoundPage = () => (
-  <Layout>
-    <SEO title="404: Not found" />
-    <MainDiv>
-      <h1>404 | Not found </h1>    
-    </MainDiv>
-  </Layout>
-)
+const NotFoundPage = () => {
+  return (
+    <ModeProvider>
+      <Layout>
+        <SEO title="404: Not found" />
+        <MainDiv>
+          <h1>404 | Not found </h1>
+        </MainDiv>
+      </Layout>
+    </ModeProvider>
+  )
+}
 
 export default NotFoundPage
--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -3,14 +3,19 @@ import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import ModeProvider from "../context/ModeContext"
 
-const SecondPage = () => (
-  <Layout>
-    <SEO title="Page two" />
-    <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
-    <Link to="/">Go back to the homepage</Link>
-  </Layout>
-)
+const SecondPage = () => {
+  return (
+    <ModeProvider>
+      <Layout>
+        <SEO title="Page two" />
+        <h1>Hi from the second page</h1>
+        <p>Welcome to page 2</p>
+        <Link to="/">Go back to the homepage</Link>
+      </Layout>
+    </ModeProvider>
+  )
+}
 
 export default SecondPage
