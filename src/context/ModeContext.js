793f0492ed9802a34c85adc966115e2ab9081e82
rodzy <49137701+rodzy@users.noreply.github.com>
2020-09-21 14:39:37 -0600

    Persist to localstorage the actual mode effect ⚡

--- src/context/ModeContext.js
+++ src/context/ModeContext.js
@@ -1,10 +1,26 @@
-import React, { useState } from "react"
+import React, { useEffect, useState } from "react"
 
 export const ModeContext = React.createContext()
 
 const ModeProvider = props => {
-  const [mode, setMode] = useState(false)
+  const [mode, setMode] = useState(initialState)
 
+  useEffect(() => {
+    localStorage.setItem("mode", JSON.stringify(mode))
+  }, [mode])
+
+  function initialState() {
+    const inStorage = "mode" in localStorage
+    const savedMode = JSON.parse(localStorage.getItem("mode"))
+    const userPreference = getColorScheme()
+    const value = inStorage ? savedMode : userPreference ? true : false
+    return value
+  }
+
+  function getColorScheme() {
+    if (!window.matchMedia) return
+    return window.matchMedia("(prefers-color-scheme:dark)").matches
+  }
   return (
     <ModeContext.Provider
       value={{
