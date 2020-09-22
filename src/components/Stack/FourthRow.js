8ff21e342e1577dee2acc90b09739f162ab96fee
rodzy <49137701+rodzy@users.noreply.github.com>
2020-09-22 16:46:59 -0600

    Removed! express icon

--- src/components/Stack/FourthRow.js
+++ src/components/Stack/FourthRow.js
@@ -1,58 +1,114 @@
-import React, { Fragment } from "react"
+import React, { Fragment, useContext } from "react"
+import { ModeContext } from "../../context/ModeContext"
 import { FOURTHDROW } from "../../data/fourthrow"
 import Card from "./CardComponent"
 
 const FourthRow = () => {
+  const { mode } = useContext(ModeContext)
   return (
     <Fragment>
-      {FOURTHDROW.map(item => (
-        <Card
-          key={item.title}
-          image={item.image}
-          title={item.title}
-          description={item.description}
-        />
-      ))}
-      {FOURTHDROW.map(item => (
-        <Card
-          key={item.title}
-          image={item.image}
-          title={item.title}
-          description={item.description}
-        />
-      ))}
-      {FOURTHDROW.map(item => (
-        <Card
-          key={item.title}
-          image={item.image}
-          title={item.title}
-          description={item.description}
-        />
-      ))}
-      {FOURTHDROW.map(item => (
-        <Card
-          key={item.title}
-          image={item.image}
-          title={item.title}
-          description={item.description}
-        />
-      ))}
-      {FOURTHDROW.map(item => (
-        <Card
-          key={item.title}
-          image={item.image}
-          title={item.title}
-          description={item.description}
-        />
-      ))}
-      {FOURTHDROW.map(item => (
-        <Card
-          key={item.title}
-          image={item.image}
-          title={item.title}
-          description={item.description}
-        />
-      ))}
+      {FOURTHDROW.map(item =>
+        item.darkmode && mode ? (
+          <Card
+            key={item.title}
+            image={item.imageDark}
+            title={item.title}
+            description={item.description}
+          />
+        ) : (
+          <Card
+            key={item.title}
+            image={item.image}
+            title={item.title}
+            description={item.description}
+          />
+        )
+      )}
+      {FOURTHDROW.map(item =>
+        item.darkmode && mode ? (
+          <Card
+            key={item.title}
+            image={item.imageDark}
+            title={item.title}
+            description={item.description}
+          />
+        ) : (
+          <Card
+            key={item.title}
+            image={item.image}
+            title={item.title}
+            description={item.description}
+          />
+        )
+      )}
+      {FOURTHDROW.map(item =>
+        item.darkmode && mode ? (
+          <Card
+            key={item.title}
+            image={item.imageDark}
+            title={item.title}
+            description={item.description}
+          />
+        ) : (
+          <Card
+            key={item.title}
+            image={item.image}
+            title={item.title}
+            description={item.description}
+          />
+        )
+      )}
+      {FOURTHDROW.map(item =>
+        item.darkmode && mode ? (
+          <Card
+            key={item.title}
+            image={item.imageDark}
+            title={item.title}
+            description={item.description}
+          />
+        ) : (
+          <Card
+            key={item.title}
+            image={item.image}
+            title={item.title}
+            description={item.description}
+          />
+        )
+      )}
+      {FOURTHDROW.map(item =>
+        item.darkmode && mode ? (
+          <Card
+            key={item.title}
+            image={item.imageDark}
+            title={item.title}
+            description={item.description}
+          />
+        ) : (
+          <Card
+            key={item.title}
+            image={item.image}
+            title={item.title}
+            description={item.description}
+          />
+        )
+      )}
+      {FOURTHDROW.map(item =>
+        item.darkmode && mode ? (
+          <Card
+            key={item.title}
+            image={item.imageDark}
+            title={item.title}
+            description={item.description}
+          />
+        ) : (
+          <Card
+            key={item.title}
+            image={item.image}
+            title={item.title}
+            description={item.description}
+          />
+        )
+      )}
     </Fragment>
   )
 }
