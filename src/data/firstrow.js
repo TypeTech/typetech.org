192faf5eed1f82d8cda88faa59ee83cc251e2920
rodzy <49137701+rodzy@users.noreply.github.com>
2020-09-22 16:31:08 -0600

    Firstrow working with darkmode icons

--- src/data/firstrow.js
+++ src/data/firstrow.js
@@ -1,5 +1,6 @@
 import reactLogo from "../images/techstack/react-2.svg"
 import nextjs from "../images/techstack/nextjs-3 (1).svg"
+import nextdark from "../images/techstack/nextjs-3-dark.svg"
 import gatsby from "../images/techstack/gatsby.svg"
 
 export const FIRSTROW = [
@@ -11,6 +12,7 @@ export const FIRSTROW = [
   },
   {
     image: nextjs,
+    imageDark: nextdark,
     title: "Nextjs",
     description: "Frontend development",
     darkmode: true,
