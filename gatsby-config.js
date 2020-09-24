fc5fa088a9ed125c225e98de13ab43f66c8b2398
rodzy <49137701+rodzy@users.noreply.github.com>
2020-09-24 13:12:23 -0600

    [Fix] Maskable icon prop

--- gatsby-config.js
+++ gatsby-config.js
@@ -41,11 +41,7 @@ module.exports = {
             src: '/icons/typetech_mask.png',
             sizes: '192x192',
             type: 'image/png',
-          },
-          {
-            src: '/icons/typetech_mask.png',
-            sizes: '512x512',
-            type: 'image/png',
+            purpose: 'maskable'
           },
         ]
       },
