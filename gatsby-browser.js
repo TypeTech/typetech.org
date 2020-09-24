b0e488ecaf5a26a8527f6d26c7b370667c7c4205
rodzy <49137701+rodzy@users.noreply.github.com>
2020-09-24 12:46:06 -0600

    Added no script tag and more optimizations to service workers

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,1 +1,11 @@
-exports.registerServiceWorker = () => true;
+export const onServiceWorkerUpdateReady = () => {
+    const answer = window.confirm(
+      `This application has been updated. ` +
+        `Reload to display the latest version?`
+    )
+    if (answer === true) {
+      window.location.reload()
+    }
+  }
+
+export const registerServiceWorker = () => true;
